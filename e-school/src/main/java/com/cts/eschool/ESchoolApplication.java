4a1585cdbab4ad508f5e6e2622f4d28f8d8d913f
tarunsistla <100066271+tarunsistla@users.noreply.github.com>
2023-11-17 04:46:16 +0530

    Update ESchoolApplication.java

--- e-school/src/main/java/com/cts/eschool/ESchoolApplication.java
+++ e-school/src/main/java/com/cts/eschool/ESchoolApplication.java
@@ -13,12 +13,13 @@ public class ESchoolApplication {
 
 		ConfigurableApplicationContext ss = SpringApplication.run(ESchoolApplication.class, args);
 		StudentService s = ss.getBean(StudentService.class);
-		s.addNewStudentDetail(1, "Tarun", 23.9);
-		s.addNewStudentDetail(2, "Yamini", 22.9);
+		s.addNewStudentDetail(1, "Tarun", 78);
+		s.addNewStudentDetail(2, "Ganesh", 45);
+		s.addNewStudentDetail(3, "Abhi", 98);
 		
 		s.getAllStudentDetails();
-		//s.getStudentDetailsById(2);
-		//s.updateStudentScore(2,50.8);
+		s.getStudentDetailsById(2);
+		s.updateStudentScore(2,50.8);
 		s.deleteStudentDetails(1);
 		s.getAllStudentDetails();
 	}
